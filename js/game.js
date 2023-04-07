ebbc4146e162b724908b800b9eb18a7d3f851d9c
unsortedAndy23 <unsortedandy@gmail.com>
2023-04-07 20:28:42 +0530

    v2.0.0: Rewrote whole code to make states easier

--- js/game.js
+++ js/game.js
@@ -32,20 +32,19 @@ const sampleProfile = {
 var game = new Phaser.Game(config);
 
 //required var
-let version = "1.6.0";
+let version = "2.0.0";
 let minePerClick = 1;
 let profile;
-let elaspedTxt, usernameTxt, ttlClicksTxt, cashTxt, invTxt;
-let gameState = 'home'
-//states :- 'home', 'shop', 'ironsmith', 'linkedin', 'antiques'
-
+let elaspedTxt, usernameTxt, ttlClicksTxt, cashTxt;
+let base; //main object
+let stuff = new Object(); //object inside
 function preload() {
 	this.load.image('background', '../assets/images/bg_cave.jpg');
 	this.load.image('mineLogo', '../assets/images/mine.png');
 	this.load.image('save','../assets/images/save.png');
 	this.load.image('ironsmith','../assets/images/ironsmith.png');
 	this.load.image('shop','../assets/images/shop.png');
-
+	
 	//loading minerals
 	this.load.image('stone','../assets/images/minerals/stone.png');
 	this.load.image('iron','../assets/images/minerals/iron.png');
@@ -53,67 +52,82 @@ function preload() {
 	this.load.image('gold','../assets/images/minerals/gold.png');
 	this.load.image('emerald','../assets/images/minerals/emerald.png');
 	this.load.image('diamond','../assets/images/minerals/diamond.png');
-
+	
 	
 	if(document.cookie){
-
+		
 		//loading existing profile from cookie
 		let deData = CryptoJS.AES.decrypt((document.cookie).substring(4), 'openkeyLOL').toString(CryptoJS.enc.Utf8)
 		profile = JSON.parse(deData);
 		console.log("loaded old profile")
 	}else{
-
+		
 		//creating a new profile
-	let enData = CryptoJS.AES.encrypt(JSON.stringify(sampleProfile), 'openkeyLOL').toString();
+		let enData = CryptoJS.AES.encrypt(JSON.stringify(sampleProfile), 'openkeyLOL').toString();
 		document.cookie = "pro="+enData;
 		profile = sampleProfile;
 		console.log("Created new key!")
 	}
-console.log(profile);
-
+	console.log(profile);
+	
 }
 
 function create() {
-	//adding background
-	let background = this.add.image(0, 0, 'background');
-	background.setOrigin(0, 0);
-	background.setScale(this.game.config.width / background.width, this.game.config.height / background.height);
-
+	//assigning base its value
+	
+	base = this;
+	loadDef()
+	setState("home")
+	
+	
 	//tick timer every 3 sec.
 	this.time.addEvent({ 
-        delay: 3 * 1000,
+		delay: 3 * 1000,
         callback: tick,
         callbackScope: this,
         loop: true
     });
 	
+}
+
+function loadDef(){
+	//clear slate
+	for(child in stuff){
+	if (child instanceof Phaser.GameObjects.Graphics || child instanceof Phaser.GameObjects.Image || child instanceof Phaser.GameObjects.Sprite || child instanceof Phaser.GameObjects.Text) {
+			child.destroy();}}
+
+/* COMMON THINGS IN ALL THE GAME STATES */
+	//adding background
+	let background = base.add.image(0, 0, 'background');
+	background.setOrigin(0, 0);
+	background.setScale(base.game.config.width / background.width, base.game.config.height / background.height);
 	
-		//save button & text
-		let saveTxt = this.add.text(30, 70, "Save")
-		this.saveBtn = this.add.rectangle(50, 50, 40, 40, 0xDBB941).setAlpha(0.7)
-		.setInteractive().on('pointerdown', save).on('pointerover', function(){
-			saveTxt.setVisible(true)
-		}).on('pointerout', function(){
-			saveTxt.setVisible(false)
-		})
-		this.add.image(this.saveBtn.x, this.saveBtn.y, 'save').setScale(0.1);
+	//save button & text
+	let saveTxt = base.add.text(30, 70, "Save")
+	base.saveBtn = base.add.rectangle(50, 50, 40, 40, 0xDBB941).setAlpha(0.7)
+	.setInteractive().on('pointerdown', save).on('pointerover', function(){
+		saveTxt.setVisible(true)
+	}).on('pointerout', function(){
+		saveTxt.setVisible(false)
+	})
+	base.add.image(base.saveBtn.x, base.saveBtn.y, 'save').setScale(0.1);
 	
-		//version
-		this.add.graphics().fillStyle(0x000000, 0.9).fillRoundedRect(100, 0, 100, 40, { tl: 0, tr: 0, bl: 12, br: 12 });
-		this.add.text(120, 20, "v" + version, {color: '#ffffff'})
+	//version
+	base.add.graphics().fillStyle(0x000000, 0.9).fillRoundedRect(100, 0, 100, 40, { tl: 0, tr: 0, bl: 12, br: 12 });
+	base.add.text(120, 20, "v" + version, {color: '#ffffff'})
 	
-		//time passed.
-		this.add.graphics().fillStyle(0x000000, 1).fillRoundedRect(480, 0, 300, 70, { tl: 0, tr: 0, bl: 30, br: 30 });
-		this.add.text(610, 25, 'Started Journey', {fontSize: "20px"}).setOrigin(0.5);
-		elaspedTxt = this.add.text(630, 50, elapseFormat(profile.hoursPassed), {fontSize: "20px",fontStyle: "bold"}).setOrigin(0.5);
+	//time passed.
+	base.add.graphics().fillStyle(0x000000, 1).fillRoundedRect(480, 0, 300, 70, { tl: 0, tr: 0, bl: 30, br: 30 });
+	base.add.text(610, 25, 'Started Journey', {fontSize: "20px"}).setOrigin(0.5);
+	elaspedTxt = base.add.text(630, 50, elapseFormat(profile.hoursPassed), {fontSize: "20px",fontStyle: "bold"}).setOrigin(0.5);
 	
-		//profile
-		this.add.graphics().fillStyle(0x59463B, 0.8).fillRoundedRect(980, 10, 340, 40, { tl: 40, tr: 0, bl: 0, br: 0 })
-		.fillStyle(0xA48675, 0.65).fillRoundedRect(1020, 50, 300, 35, { tl: 0, tr: 0, bl: 30, br: 0 })
-		.fillStyle(0x31EB5A, 0.65).fillRoundedRect(1120, 85, 200, 30, { tl: 0, tr: 0, bl: 30, br: 0 });
-		
-		usernameTxt = this.add.text(1120, 30, "@"+profile.name, {fontSize: "40px",fontStyle: "bold"}).setOrigin(0.5)
-		.setInteractive().on('pointerdown', function(){
+	//profile
+	base.add.graphics().fillStyle(0x59463B, 0.8).fillRoundedRect(980, 10, 340, 40, { tl: 40, tr: 0, bl: 0, br: 0 })
+	.fillStyle(0xA48675, 0.65).fillRoundedRect(1020, 50, 300, 35, { tl: 0, tr: 0, bl: 30, br: 0 })
+	.fillStyle(0x31EB5A, 0.65).fillRoundedRect(1120, 85, 200, 30, { tl: 0, tr: 0, bl: 30, br: 0 });
+	
+	usernameTxt = base.add.text(1120, 30, "@"+profile.name, {fontSize: "40px",fontStyle: "bold"}).setOrigin(0.5)
+	.setInteractive().on('pointerdown', function(){
 		//validation & limiting renaming to once only
 		let newName = prompt("provide a new username").toString().trim()
 		if((newName.length>12)||newName.length == 0) return alert("Please provide a username with 12 or less characters.")
@@ -123,13 +137,31 @@ function create() {
 		profile.name = newName;
 		usernameTxt.text = "@"+ newName;
 		save()
-		})
+	})
+	
+	ttlClicksTxt = base.add.text(1160, 65, profile.clicks + " total clicks", {fontSize: "26px"}).setOrigin(0.5)
+	cashTxt = base.add.text(1180, 100, profile.balance + " $", {fontSize: "26px", fontStyle:"bold", fill:'#0F710D'}).setOrigin(0.5)
+}
 
+function setState(state){
+	console.log("State changed to " + state)
+	//clean the canvas
+	loadDef()	
+	
+	//states :- 'home', 'shop', 'ironsmith', 'linkedin', 'antiques'
+	if(state === 'home') home();
+	else if(state === 'shop') return;//shop
+	else if(state === 'ironsmith') return; //ironsmith
+}
 
-if(gameState==='home'){
+function update() {
+	
+}
+
+function home(){
 	//mine button
-	this.add.graphics().fillStyle(0xC36D1D, 0.22).fillCircle(680, 300, 70).fillCircle(680, 300, 90)
-	this.mineBtn = this.add.sprite(684, 300, 'mineLogo').setScale(0.20).setInteractive()
+	stuff.mineBg = base.add.graphics().fillStyle(0xC36D1D, 0.22).fillCircle(680, 300, 70).fillCircle(680, 300, 90)
+	stuff.mineBtn = base.add.sprite(684, 300, 'mineLogo').setScale(0.20).setInteractive()
 	.on('pointerdown', function() {
 	  this.scale -= 0.05;
 	  //add clicks
@@ -138,38 +170,28 @@ if(gameState==='home'){
 	});
 
 	//ironsmith icon
-	let ironsmTxt = this.add.text(30, 230, "Ironsmith",{fontSize: "20px"})
-		this.ironsBtn = this.add.rectangle(70, 180, 80, 80, 0xDEBB78).setAlpha(0.56)
-		.setInteractive().on('pointerdown', function(){gameState='ironsmith'}).on('pointerover', function(){
-		ironsmTxt.setVisible(true)}).on('pointerout', function(){ironsmTxt.setVisible(false)})
-		this.add.image(this.ironsBtn.x, this.ironsBtn.y, 'ironsmith').setScale(4);
+	stuff.ironsmTxt = base.add.text(30, 230, "Ironsmith",{fontSize: "20px"})
+	stuff.ironsmBtn = base.add.rectangle(70, 180, 80, 80, 0xDEBB78).setAlpha(0.56)
+		.setInteractive().on('pointerdown', function(){setState('ironsmith')}).on('pointerover', function(){
+			stuff.ironsmTxt.setVisible(true)}).on('pointerout', function(){stuff.ironsmTxt.setVisible(false)})
+		stuff.ironsmImg = base.add.image(stuff.ironsmBtn.x, stuff.ironsmBtn.y, 'ironsmith').setScale(4);
 	
 	//shop icon
-	let shopTxt = this.add.text(1080, 690, "Shop",{fontSize: "20px"})
-		let shopImg = this.add.image(1180, 640, 'shop').setScale(0.3)
-		.setInteractive().on('pointerdown', function(){gameState='shop'}).on('pointerover', function(){
-		shopTxt.setVisible(true);this.setScale(0.4)}).on('pointerout', function(){shopTxt.setVisible(false);this.setScale(0.3)})
+	stuff.shopTxt = base.add.text(1080, 690, "Shop",{fontSize: "20px"})
+	stuff.shopImg = base.add.image(1180, 640, 'shop').setScale(0.3)
+		.setInteractive().on('pointerdown', function(){setState('shop')}).on('pointerover', function(){
+			stuff.shopTxt.setVisible(true);this.setScale(0.4)}).on('pointerout', function(){stuff.shopTxt.setVisible(false);this.setScale(0.3)})
 
 	//Inventory
-	this.add.graphics().fillStyle(0x634C46, 0.67).fillRoundedRect(1040, 140, 220, 340, { tl: 40, tr: 40, bl: 0, br: 0 })
-	this.add.text(1070, 150, "Inventory",{fontSize: "30px"})
-	invTxt = this.add.text(1140, 200, `${profile.res.stone}\n\n${profile.res.iron}\n\n${profile.res.copper}\n\n${profile.res.gold}\n\n${profile.res.emerald}\n\n${profile.res.diamond}`,{fontSize: "24px"})
-	this.add.image(1100, 200, 'stone').setScale(0.4)
-	this.add.image(1100, 250, 'iron').setScale(0.4)
-	this.add.image(1100, 300, 'copper').setScale(0.4)
-	this.add.image(1100, 350, 'gold').setScale(0.4)
-	this.add.image(1100, 400, 'emerald').setScale(0.4)
-	this.add.image(1100, 450, 'diamond').setScale(0.4)
-}
-	
-
-	ttlClicksTxt = this.add.text(1160, 65, profile.clicks + " total clicks", {fontSize: "26px"}).setOrigin(0.5)
-	cashTxt = this.add.text(1180, 100, profile.balance + " $", {fontSize: "26px", fontStyle:"bold", fill:'#0F710D'}).setOrigin(0.5)
-
-}
-
-function update() {
-
+	stuff.invRect = base.add.graphics().fillStyle(0x634C46, 0.67).fillRoundedRect(1040, 140, 220, 340, { tl: 40, tr: 40, bl: 0, br: 0 })
+	stuff.invTitle = base.add.text(1070, 150, "Inventory",{fontSize: "30px"})
+	stuff.invTxt = base.add.text(1140, 200, `${profile.res.stone}\n\n${profile.res.iron}\n\n${profile.res.copper}\n\n${profile.res.gold}\n\n${profile.res.emerald}\n\n${profile.res.diamond}`,{fontSize: "24px"})
+	stuff.min1 = base.add.image(1100, 200, 'stone').setScale(0.4)
+	stuff.min2 = base.add.image(1100, 250, 'iron').setScale(0.4)
+	stuff.min3 = base.add.image(1100, 300, 'copper').setScale(0.4)
+	stuff.min4 = base.add.image(1100, 350, 'gold').setScale(0.4)
+	stuff.min5 = base.add.image(1100, 400, 'emerald').setScale(0.4)
+	stuff.min6 = base.add.image(1100, 450, 'diamond').setScale(0.4)
 }
 
 //runs every x seconds
@@ -221,7 +243,7 @@ function click(){
 
 function updateInv(){
 	let min = profile.res
-	invTxt.text = `${min.stone}\n\n${min.iron}\n\n${min.copper}\n\n${min.gold}\n\n${min.emerald}\n\n${min.diamond}`
+	stuff.invTxt.text = `${min.stone}\n\n${min.iron}\n\n${min.copper}\n\n${min.gold}\n\n${min.emerald}\n\n${min.diamond}`
 }
 
 function save(){
